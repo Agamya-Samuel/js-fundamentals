166280059ec79371e2c394e30a85997cbe7b0b27
Agamya Samuel <48693970+Agamya-Samuel@users.noreply.github.com>
2023-10-26 23:56:51 +0530

    Update 13-async-await.js

--- 13-async-await.js
+++ 13-async-await.js
@@ -1,6 +1,3 @@
-// Source Website: https://www.youtube.com/watch?v=vn3tm0quoqE
-// Live Website: https://jsbin.com/ceyuyuq/edit?js,console
-
 // Example 1
 
 // using Promises with async/await
@@ -61,10 +58,49 @@ main();
 // 2. async/await only affects Promise-receiver, Promise-maker side remains as it is
 // 3. You can await any function that returns a Promise, like json(), fetch(), also
 // 4. Any function can be converted to async
-// const me =
-// sayHello()
-// return
-// •I am Qoli'
-// }
-// }
-// me. sayHello() //'I am Qoli'
+// 5. All async functions return a Promise. If you Explicitly return a value (a string. or a number), it will still return a Promise, but a Resolved Promise with the value you returned
+// 6. Error handling - using try/catch block. As we don't have catch block in async/await, we use classic old-school: try/catch block
+// 7. Another advantage of async/await is that keeps the onSuccessful and onFailure code separate, which makes the code more readable and maintainable
+
+// Example 3
+
+function getData() {
+	return new Promise(function (resolve, reject) {
+		setTimeout(() => {
+			// resolve ('Wish you Happy Birthday!')
+			reject('Something went wrong!');
+		}, 1);
+	});
+}
+
+function onSuccess(data) {
+	console.log(`SUCCESS: ${data}`);
+}
+
+function onFailure(err) {
+	console.log(`ERROR: ${err}`);
+}
+
+// using try/catch block
+async function start1() {
+	try {
+		const result = await getData();
+		onSuccess(result);
+	} catch (error) {
+		onFailure(error);
+	}
+}
+
+// using .then() and .catch()
+async function start2() {
+	const result = await getData()
+		.then((result) => {
+			console.log(`SUCCESS: ${result}`);
+		})
+		.catch((error) => {
+			console.log(`ERROR: ${error}`);
+		});
+}
+
+start1();
+start2();
